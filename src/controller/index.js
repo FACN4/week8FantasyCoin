39ce0c338e9c8dce907c6bba38238e7974ec9914
DenisKent <kentdenis6@hotmail.co.uk>
2018-08-23 16:44:12 +0300

    Router working

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 
 const error = require('./error');
@@ -5,6 +6,9 @@ const login = require('./login');
 const signup = require('./signup');
 const profile = require('./profile');
 const trading = require('./trading');
+const checkLogin = require('./checkLogin');
+const newUser = require('./newUser');
+const exchange = require('./exchange');
 
 const router = express.Router();
 
@@ -13,13 +17,22 @@ router.use((req, res, next) => {
   next();
 });
 
+// Serve static pages
 router.get('/', login.get);
 router.get('/signup', signup.get);
+
+// Serve dynamic pages
 router.get('/profile/:user', profile.get);
 router.get('/trading', trading.get);
 
+// Serve post requests
+router.post('/checkLogin', checkLogin.post);
+router.post('/newUser', newUser.post);
+router.post('/exchange', exchange.post);
+
+
+// Error handling
 router.use(error.client);
 router.use(error.server);
-
 
 module.exports = router;
